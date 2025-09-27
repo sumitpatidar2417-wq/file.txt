93eac3ffb4d8b85bae67bc0f0d90bdf2f31cff66
sumitpatidar2417-wq <sumitpatidar2417@gmail.com>
2025-09-27 12:57:31 +0530

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,5 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+#print("(50 - 5*6) / 4")
+("(50 - 5*6) / 4")
+print("hello world ")
